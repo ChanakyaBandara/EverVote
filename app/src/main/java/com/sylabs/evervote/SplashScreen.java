73198be859ff6f07137993989842f3a328514c13
Malih Ileperuma <35-cs-0002@kdu.ac.lk>
2021-10-16 23:44:14 +0530

    fixed profile section , card image

--- app/src/main/java/com/sylabs/evervote/SplashScreen.java
+++ app/src/main/java/com/sylabs/evervote/SplashScreen.java
@@ -2,13 +2,36 @@ package com.sylabs.evervote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private ImageView view3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        view3 = (ImageView) findViewById(R.id.logoimg);
+
+        final Intent intent = new Intent(this, Login.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }
