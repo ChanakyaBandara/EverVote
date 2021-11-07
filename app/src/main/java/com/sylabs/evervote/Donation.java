91ba6cc7b801fecf2ef0ed5d67dd998ad579c726
Malih Ileperuma <35-cs-0002@kdu.ac.lk>
2021-11-07 12:31:33 +0530

    changed candidate profile section with fragments

--- app/src/main/java/com/sylabs/evervote/Donation.java
+++ app/src/main/java/com/sylabs/evervote/Donation.java
@@ -20,52 +20,52 @@ import static com.sylabs.evervote.MainActivity.UserName;
 
 public class Donation extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    private DatabaseReference mUserDatabase;
-    private TextView txtDonationTitle,txtDonationContent,txtDonationAmount;
-    private String CID,CName;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_donation);
-
-        mAuth = FirebaseAuth.getInstance();
-
-        txtDonationTitle = (TextView) findViewById(R.id.txtDonationTitle);
-        txtDonationAmount = (TextView) findViewById(R.id.txtDonationAmount);
-        txtDonationContent = (TextView) findViewById(R.id.txtDonationContent);
-
-        String userId = mAuth.getCurrentUser().getUid();
-
-        CID = getIntent().getStringExtra("CID");
-        CName = getIntent().getStringExtra("CName");
-    }
-
-    public void makeDonaton(View view) {
-        String formattedDate ="";
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            LocalDateTime localDateTime = LocalDateTime.now();
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            formattedDate = localDateTime.format(dateTimeFormatter);
-        }
-
-        final String DTitle = txtDonationTitle.getText().toString();
-        final String DAmount = txtDonationAmount.getText().toString();
-        final String DContent = txtDonationContent.getText().toString();
-        final String timeStamp = formattedDate;
-
-        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Donation").push();
-        Map Donation = new HashMap<>();
-        Donation.put("DTitle", DTitle);
-        Donation.put("DAmount", DAmount);
-        Donation.put("DContent", DContent);
-        Donation.put("DDonner", UserName);
-        Donation.put("CID", CID);
-        Donation.put("CName", CName);
-        Donation.put("timeStamp", timeStamp);
-        mUserDatabase.updateChildren(Donation);
-
-        Toast.makeText(Donation.this, "Successfully Added", Toast.LENGTH_SHORT).show();
-    }
+//    private FirebaseAuth mAuth;
+//    private DatabaseReference mUserDatabase;
+//    private TextView txtDonationTitle,txtDonationContent,txtDonationAmount;
+//    private String CID,CName;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_donation);
+//
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        txtDonationTitle = (TextView) findViewById(R.id.txtDonationTitle);
+//        txtDonationAmount = (TextView) findViewById(R.id.txtDonationAmount);
+//        txtDonationContent = (TextView) findViewById(R.id.txtDonationContent);
+//
+//        String userId = mAuth.getCurrentUser().getUid();
+//
+//        CID = getIntent().getStringExtra("CID");
+//        CName = getIntent().getStringExtra("CName");
+//    }
+//
+//    public void makeDonaton(View view) {
+//        String formattedDate ="";
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+//            LocalDateTime localDateTime = LocalDateTime.now();
+//            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//            formattedDate = localDateTime.format(dateTimeFormatter);
+//        }
+//
+//        final String DTitle = txtDonationTitle.getText().toString();
+//        final String DAmount = txtDonationAmount.getText().toString();
+//        final String DContent = txtDonationContent.getText().toString();
+//        final String timeStamp = formattedDate;
+//
+//        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Donation").push();
+//        Map Donation = new HashMap<>();
+//        Donation.put("DTitle", DTitle);
+//        Donation.put("DAmount", DAmount);
+//        Donation.put("DContent", DContent);
+//        Donation.put("DDonner", UserName);
+//        Donation.put("CID", CID);
+//        Donation.put("CName", CName);
+//        Donation.put("timeStamp", timeStamp);
+//        mUserDatabase.updateChildren(Donation);
+//
+//        Toast.makeText(Donation.this, "Successfully Added", Toast.LENGTH_SHORT).show();
+//    }
 }
--- app/src/main/java/com/sylabs/evervote/Feedback.java
+++ app/src/main/java/com/sylabs/evervote/Feedback.java
@@ -20,49 +20,49 @@ import static com.sylabs.evervote.MainActivity.UserName;
 
 public class Feedback extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    private DatabaseReference mUserDatabase;
-    private TextView txtFeedbackTitle,txtFeedbackContent;
-    private String CID,CName;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feedback);
-
-        mAuth = FirebaseAuth.getInstance();
-
-        txtFeedbackTitle = (TextView) findViewById(R.id.txtFeedbackTitle);
-        txtFeedbackContent = (TextView) findViewById(R.id.txtFeedbackContent);
-
-        String userId = mAuth.getCurrentUser().getUid();
-
-        CID = getIntent().getStringExtra("CID");
-        CName = getIntent().getStringExtra("CName");
-    }
-
-    public void sendFeedback(View view) {
-        String formattedDate ="";
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            LocalDateTime localDateTime = LocalDateTime.now();
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            formattedDate = localDateTime.format(dateTimeFormatter);
-        }
-
-        final String FTitle = txtFeedbackTitle.getText().toString();
-        final String FContent = txtFeedbackContent.getText().toString();
-        final String timeStamp = formattedDate;
-
-        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Feedback").push();
-        Map Feedback = new HashMap<>();
-        Feedback.put("FTitle", FTitle);
-        Feedback.put("FContent", FContent);
-        Feedback.put("DComplainer", UserName);
-        Feedback.put("timeStamp", timeStamp);
-        Feedback.put("CID", CID);
-        Feedback.put("CName", CName);
-        mUserDatabase.updateChildren(Feedback);
-
-        Toast.makeText(Feedback.this, "Successfully Added", Toast.LENGTH_SHORT).show();
-    }
+//    private FirebaseAuth mAuth;
+//    private DatabaseReference mUserDatabase;
+//    private TextView txtFeedbackTitle,txtFeedbackContent;
+//    private String CID,CName;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_feedback);
+//
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        txtFeedbackTitle = (TextView) findViewById(R.id.txtFeedbackTitle);
+//        txtFeedbackContent = (TextView) findViewById(R.id.txtFeedbackContent);
+//
+//        String userId = mAuth.getCurrentUser().getUid();
+//
+//        CID = getIntent().getStringExtra("CID");
+//        CName = getIntent().getStringExtra("CName");
+//    }
+//
+//    public void sendFeedback(View view) {
+//        String formattedDate ="";
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+//            LocalDateTime localDateTime = LocalDateTime.now();
+//            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//            formattedDate = localDateTime.format(dateTimeFormatter);
+//        }
+//
+//        final String FTitle = txtFeedbackTitle.getText().toString();
+//        final String FContent = txtFeedbackContent.getText().toString();
+//        final String timeStamp = formattedDate;
+//
+//        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Feedback").push();
+//        Map Feedback = new HashMap<>();
+//        Feedback.put("FTitle", FTitle);
+//        Feedback.put("FContent", FContent);
+//        Feedback.put("DComplainer", UserName);
+//        Feedback.put("timeStamp", timeStamp);
+//        Feedback.put("CID", CID);
+//        Feedback.put("CName", CName);
+//        mUserDatabase.updateChildren(Feedback);
+//
+//        Toast.makeText(Feedback.this, "Successfully Added", Toast.LENGTH_SHORT).show();
+//    }
 }
